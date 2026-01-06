51063a7cfefadfa6c5d1a962caaecff546432d7a
Goran Ferbišek <goran.ferbisek@gmail.com>
2026-01-06 19:51:07 +0100

    Define routes with elements

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,22 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import App from './App.jsx'
 import Home from "./components/pages/Home.jsx"
 import Login from "./components/pages/Login.jsx"
 import Signup from "./components/pages/Signup.jsx";
 import ErrorPage from "./components/pages/ErrorPage.jsx";
 
-const appRouter = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>,
-        errorElement: <ErrorPage/>,
-        children: [
-            {
-                index: true,
-                element: <Home/>
-            },{
-                path: "/signup",
-                element: <Signup/>
-            },
-            {
-                path: "/login",
-                element: <Login/>
-            }
-        ]
-    },
-]);
+const routeDefinitions = createRoutesFromElements(
+  <Route path="/" element={<App/>} errorElement={<ErrorPage/>}>
+      <Route index element={<Home/>} />
+      <Route path="/signup" element={<Signup/>} />
+      <Route path="/login" element={<Login/>} />
+  </Route>
+);
+
+const appRouter = createBrowserRouter(routeDefinitions);
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
